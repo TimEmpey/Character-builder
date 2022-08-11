42b4436abc00e76fadeff6d6cae51501d38b6ed1
Tim Empey <timothyempey@gmail.com>
2022-08-11 13:35:39 -0700

    add variables to js

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+const spcecies = document.getElementById("input#species").value;
+const trade = document.getElementById("input#trade").value;
+const gender = document.getElementById("input#gender").value;
+const dob = document.getElementById("input#born").value;
+const favColor = document.getElementById("input#born").value;
