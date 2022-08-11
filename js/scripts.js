643a44d8264aa3dddcabbdbc9b24e1b8e2cfc9e2
Tim Empey <timothyempey@gmail.com>
2022-08-11 13:49:04 -0700

    add if then statement
    Co-authored-by: Athea DeLing <almdeling@gmail.com>
    Co-authored-by: Harold Mesa <harold.mesa@icloud.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,24 @@
-const spcecies = document.getElementById("input#species").value;
-const trade = document.getElementById("input#trade").value;
-const gender = document.getElementById("input#gender").value;
-const dob = document.getElementById("input#born").value;
-const favColor = document.getElementById("input#born").value;
+//User Interface Logic
+//window.addEventListener("load", 
+
+function handleGeneration(event){
+  event.preventDefault();
+  const species = document.getElementById("species").value;
+  const trade = document.getElementById("trade").value;
+  const gender = document.getElementById("gender").value;
+  const favColor = document.getElementById("color").value;
+  const dob = document.getElementById("born").value;
+
+
+window.addEventListener("load", function () {
+  document.getElementById("characterSelect").addEventListener("submit", handleGeneration)
+});
+
+let result;
+if (species === "Elf") {
+    document.getElementById("characterResults").removeAttribute("class");
+}
+
+document.getElementById("output").innerText = result;
+
+};
